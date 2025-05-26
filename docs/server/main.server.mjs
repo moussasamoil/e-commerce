d2a383c9d8cf9c5724cc0a67d3df20cc283e5271
moussa samoil <moussasamoil10@gmail.com>
2025-05-27 02:49:56 +0300

    build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GW2444WX.mjs";import"./chunk-G5RTC2P6.mjs";import"./chunk-SLMTYGH3.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-RDISAKEQ.mjs";import"./chunk-G5RTC2P6.mjs";import"./chunk-SLMTYGH3.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
